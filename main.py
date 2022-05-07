f73defa5a7bf44257a154108f237f96676383d5d
wpted <silverhandsome@gmail.com>
2022-05-07 11:19:53 +0800

    Create a flashy name card site.

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+server = Flask(__name__)
+
+
+@server.route("/")
+def main_page():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    server.run(debug=True)
